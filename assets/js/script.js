f1fe210cc3349140a34f1d30ee2b85a42d787f06
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-02-22 15:57:44 -0600

    started on javascript; added event listener to call main function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,11 @@
 const apiKey = "22a2a48d05ae9297e09f118308f99d21";
+const search = document.querySelector("#user-input")
+const searchBtn = document.querySelector("#search-button")
 
+
+searchBtn.addEventListener("click", function (event) {
+    event.preventDefault()
+    let city = search.value
+    getWeather(city)
+    localStorage.setItem("city", JSON.stringify(city))
+})
