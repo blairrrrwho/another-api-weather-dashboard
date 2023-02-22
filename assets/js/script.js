814e7b8b496dfa43124905d82dc635dfab4209df
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-02-22 16:03:41 -0600

    did the same thing for the five day forecast; added dates, weather data, and appended all of it to the html page

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,10 +34,35 @@ function getWeather(city) {
             temp.innerHTML = "Temp: " + Math.floor(data.main.temp) + ` &#176F`
             wind.innerHTML = "Wind: " + Math.floor(data.wind.speed) + " mph"
             humidity.innerHTML = " Humidity: " + Math.floor(data.main.humidity) + "%";
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=` + city + `&appid=` + apiKey + `&units=imperial`)
-        .then((res) => {
-            return res.json()
+            fetch(`https://api.openweathermap.org/data/2.5/forecast?q=` + city + `&appid=` + apiKey + `&units=imperial`)
+                .then((res) => {
+                    return res.json()
+                })
+                .then((data) => {
+                    console.log(data)
+                    const forecastCards = document.querySelectorAll(".card-body")
+                    for (let i = 0; i < forecastCards.length; i++) {
+                        forecastCards[i].innerHTML = "";
+                        const index = i * 8 + 4;
+                        const forecastDate = new Date(data.list[index].dt * 1000)
+                        const daysDate = document.createElement("p")
+                        const month = forecastDate.getMonth() + 1;
+                        daysDate.innerHTML = "(" + month + "/" + forecastDate.getDate() + "/" + forecastDate.getFullYear() + ")"
+                        forecastCards[i].append(daysDate)
+                        const forecastPic = document.createElement("img");
+                        forecastPic.setAttribute("src", "https://openweathermap.org/img/wn/" + data.list[index].weather[0].icon + "@2x.png");
+                        forecastPic.setAttribute("alt", data.list[index].weather[0].description);
+                        forecastCards[i].append(forecastPic);
+                        const forcastTemp = document.createElement("li")
+                        forcastTemp.innerHTML = "Temp: " + Math.floor(data.list[index].main.temp) + ` &#176F`;
+                        forecastCards[i].append(forcastTemp)
+                        const forcastWind = document.createElement("li")
+                        forcastWind.innerHTML = "Wind: " + Math.floor(data.list[index].wind.speed) + ` mph`;
+                        forecastCards[i].append(forcastWind)
+                        const forcastHumidity = document.createElement("li")
+                        forcastHumidity.innerHTML = "Humidity: " + Math.floor(data.list[index].main.humidity) + `%`;
+                        forecastCards[i].append(forcastHumidity)
+                    }
+                })
         })
-        .then((data) => {
-            console.log(data)
-        })
+}
