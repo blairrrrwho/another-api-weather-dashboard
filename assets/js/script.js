2990947d663dd0150f6dbf79875d40db4a35bc36
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-02-23 21:00:22 -0600

    conslidated repetitiveness; moved around blocks of code;

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,13 +9,26 @@ const humidity = document.querySelector("#main-card-humidity");
 const date = document.querySelector(".date");
 
 const userSearch = document.querySelector("#user-input");
-const newCityInput = document.getElementById('previousCityInput');
 // previousCityInput.innerHTML = cityInput.value;
+const newCityInput = document.getElementById('previousCityInput');
 
-let city = "";
-const searchList = document.querySelector("#results-div");
-var cityArray = JSON.parse(localStorage.getItem("city")) || [];
 
+// Main even that calls the big function that searches for the city and brings back the weather data
+// const inputBar = document.getElementById("user-input");
+// inputBar.addEventListener('keypress', function (e) {
+//     if (e.key === "Enter")
+
+searchBtn.addEventListener("click", function (event) {
+    event.preventDefault()
+    let city = userSearch.value
+    getWeatherAPI(city);
+    console.log(city, cityArray);
+    cityArray.push(city);
+    localStorage.setItem("city", JSON.stringify(cityArray));
+
+})
+
+let getCurrentWeather = `${currentWeatherEndpt}${userSearch}'&appid='${apiKey} + &units=imperial`;
 
 let searchBtn = document.getElementById('searchBtn');
 searchBtn.addEventListener('click', getUserCity);
@@ -26,11 +39,12 @@ recentBtn.addEventListener('click', (event) => choiceClicked(event));
 let clearHistoryBtn = document.getElementById('clear-history-btn');
 clearHistoryBtn.addEventListener('click', clearStorage);
 
+// A recenet search button click calls this function
 function choiceClicked(event) {
     getCoordinates(event.target.innerHTML);
 }
 
-
+// Search button calls this function
 function getUserCity() {
     // User input from input form
     if (!userSearch.value) {
@@ -45,59 +59,88 @@ function getUserCity() {
     getCoordinates(search);
 }
 
+// A recent search button click will also call this function 
+// Use Geocoding API Call and set up a function put it in a variable
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+// fetch it to turn the data into an object by using JSON.parse() or with fetch you can just use json()
+// Getting lat and lon to put into our regular current and five day weather api calls
 function getCoordinates(search) {
-    // clear out the input field
+    // Clear out the input field
     userSearch.value = '';
-    //add the variable in the parameters to pass it like a hand off
-    let apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=5&appid=${weatherKey}`;
+    // Add the variable in the parameters to pass it in -- city the user inputs
+    let apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=5&appid=${apiKey}`;
 
     fetch(apiUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            //pass the information into the next function
-            // take all the data with you and THEN pull out what you need
+            // Pass the information into the next function
+            // Take all the data with you and THEN pull out what you need
             getWeatherAPI(data[0]);
         })
         .catch((error) => {
             console.log(error)
         });
 }
+// This data should return the lat and log data but not the weather data
 
-// Main even that calls the big function that searches for the city and brings back the weather data
-searchBtn.addEventListener("click", function (event) {
-    event.preventDefault()
-    let city = userSearch.value
-    getWeatherAPI(city);
-    console.log(city, cityArray);
-    cityArray.push(city);
-    localStorage.setItem("city", JSON.stringify(cityArray));
-
-})
-
-function createButtons() {
-    for (let index = 0; index < cityArray.length; index++) {
-        // const buttonNametextContent = cityArray[index];
-
-
-
-
-    }
+// Search button click calls this function at the top
+// Be specific to carry only certain parts of the JSON
+async function getWeatherApi(location) {
+    // Deconstruct to get properties needed
+    let { lat, lon } = location;
+    let city = location.name;
+    let response = await fetch(
+        // Five-Day Forecase API url
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
+    );
+    let data = await response.json();
+    // Need to solve for current and 5 day
+    // Carry the data and split it in a funciton
+    forecastWeather(city, data.list);
 }
+
+function forecastWeather(city, list) {
+    // Empty the recent buttons
+    recentBtn.innerHTML = '';
+    // Declare the local storage array
+    let cityArray = JSON.parse(window.localStorage.getItem('previousCities')) || [];
+
+    // Set to local storage and add to the array
+    // If item does NOT exist
+    if (!cityArray.includes(city)) {
+        // Add (push) into the array
+        cityArray.push(city);
+        // When sending to local storage, must stringify first
+        window.localStorage.setItem('previousCities', JSON.stringify(cityArray));
+    
+        // create the recent buttons
+        for (let index = 0; index < cityArray.length; index++) 
+            recentBtn.innerHTML += `<button class=:"">${cityArray[index]}</button>`;
+    } else {
+        for (let index = 0; index < cityArray.length; index++) {
+            let recentBtn = document.getElementById('recent-searches-btns');
+
+            recentBtn.innerHTML += `<button class="">${cityArray[index]}</button>`;
+        }
+    };
+}
+
+    // Create clear history buttton
+    clearHistoryBtn.innerHTML = `<button class="d-grid pt-4 pb-5 btn btn-outline-secondary">Clear Search History</button>`;
+
+    
+
+// let city = "";
+// const searchList = document.querySelector("#results-div");
+// var cityArray = JSON.parse(localStorage.getItem("city")) || [];
 
 
 // create a function that uses a loop to create buttons using the city names from cityArray 
 // for each, document.createElement, .textContent, append
 // add event listener inside the same for each loop
 
-
-// Add event listener for the dynamically generated search history buttons
-
-// Create clear history buttton
-clearHistoryBtn.innerHTML = `
-<button class="d-grid pt-4 pb-5 btn btn-outline-secondary">Clear Search History</button>
-`;
 
 
 // 1st API call is to get the CURRENT weather data for the CURRENT day for a specific city 
